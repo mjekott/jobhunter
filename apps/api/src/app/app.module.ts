0ae57ad3fa39bdfc0ad9afe56381ebc6a9adaff6
mjekott <ekottmfon@yahoo.com>
2023-01-04 17:39:04 +0100

    feat(api): setup environmental variables
    install nest config and joi to implement the setup of environamental variable and validation of
    variables

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,10 @@
+import { ApiCoreModule } from '@jobhunter/api/core'
 import { Module } from '@nestjs/common'
-
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
 @Module({
-  imports: [],
+  imports: [ApiCoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
